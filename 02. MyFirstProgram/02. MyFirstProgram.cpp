2eabe880f2a9937702bc1229840d3b02bf85ca0a
Sébastien Putier <sputier@gmail.com>
2019-12-20 22:03:22 +0100

    Add 02. MyFirstProgram

---
+++ 02. MyFirstProgram/02. MyFirstProgram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter a number :" << std::endl;
+	
+	int userValue;
+	std::cin >> userValue;
+	std::cout << userValue << " + 2 = " << userValue + 2 << std::endl;
+}
