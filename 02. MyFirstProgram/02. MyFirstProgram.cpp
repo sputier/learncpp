1d9ac4cee7c15b0d72de696196f02d8db4fe08fe
Sébastien Putier <sputier@gmail.com>
2019-12-20 22:20:55 +0100

    Change the operation from 'userValue + 2' to addition of two user-defined values

--- 02. MyFirstProgram/02. MyFirstProgram.cpp
+++ 02. MyFirstProgram/02. MyFirstProgram.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
-int main()
+int getUserValue()
 {
 	std::cout << "Enter a number :" << std::endl;
-	
+
 	int userValue;
 	std::cin >> userValue;
-	std::cout << userValue << " + 2 = " << userValue + 2 << std::endl;
+
+	return userValue;
+}
+
+int main()
+{	
+	int userValue1{ getUserValue() };
+	int userValue2{ getUserValue() };
+
+	std::cout << userValue1 << " + " << userValue2 << " = " << userValue1 + userValue2<< std::endl;
 }
