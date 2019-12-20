4a2fb2ad91baf66880f279472a943fdbd7a79ff9
Sébastien Putier <sputier@gmail.com>
2019-12-20 23:31:56 +0100

    Use forward declarations

--- 02. MyFirstProgram/02. MyFirstProgram.cpp
+++ 02. MyFirstProgram/02. MyFirstProgram.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+int getUserValue();
+void printAddition(int value1, int value2);
+
+int main()
+{	
+	int userValue1{ getUserValue() };
+	int userValue2{ getUserValue() };
+
+	printAddition(userValue1, userValue2);
+}
+
 int getUserValue()
 {
 	std::cout << "Enter a number :" << std::endl;
@@ -14,12 +25,4 @@ void printAddition(int value1, int value2)
 {
 	std::cout << value1 << " + " << value2 << " = " << value1 + value2 << std::endl;
 
-}
-
-int main()
-{	
-	int userValue1{ getUserValue() };
-	int userValue2{ getUserValue() };
-
-	printAddition(userValue1, userValue2);
 }
